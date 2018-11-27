701b1652d3ac6fc2a1189ea854ba0ae0aaeaf912
Allen Wang <awang02@stuy.edu>
2018-11-27 05:33:42 -0500

    constructor and two methods

---
+++ MyString.java
@@ -0,0 +1,16 @@
+public class MyString implements CharSequence,Comparable<CharSequence>{
+  private char[] data;
+
+  public MyString(CharSequence s){
+    data = new char(s.length());
+  }
+  public int length(){
+    int count = 0;
+    for (i = 0; i < data.length; i++) {
+
+    }
+  }
+
+  }
+  }
+}
